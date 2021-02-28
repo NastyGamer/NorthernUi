56f7daebf92a3ca42bb3383adf6cdaccb5e512fd
NastyGamer <jones.m.987654321@gmail.com>
2021-02-28 13:28:36 +0100

    Added makefile for servers.json

---
+++ src/main/resources/servers/make.sh
@@ -0,0 +1,18 @@
+set -e
+
+if [ ! -d "env" ]; then
+  echo "Creating env"
+  python3 -m venv env
+  source env/bin/activate
+  echo "Installing dependencies"
+  pip install beautifulsoup4
+  pip install iso3166
+  pip install requests
+fi
+
+echo "Activating env"
+source env/bin/activate
+echo "Creating serverlist"
+python3 make_serverlist.py
+echo "Done!"
+source env/bin/deactivate
